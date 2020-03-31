3122794b52acb8176bbabcc174cef06a56ed1416
Willyan Santos <willyan41@hotmail.com>
2020-03-31 13:00:09 -0300

    Adicionar arquivos de projeto.

---
+++ Cap05/ContaBancaria.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Cap05
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double ValorConta { get; private set; }
+
+        public ContaBancaria(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+        }
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double valorConta) : this(numeroConta, nomeTitular)
+        {
+            ValorConta = valorConta;
+
+        }
+        
+        public void Deposito(double deposito)
+        {
+            ValorConta = ValorConta + deposito;
+        }
+
+        public int PegarConta(int pegarconta)
+        {
+            NumeroConta = pegarconta;
+
+            return NumeroConta;
+        }
+
+        public void Saque(double saque)
+        {
+            if (ValorConta != null && ValorConta >= saque)
+            {
+                ValorConta = ValorConta - saque - 5.00;
+            }
+            else
+            {
+                Console.WriteLine("Não possui saldo suficiente para esta operação.");
+            }
+        }
+
+        public string ToString()
+        {
+            return "Conta " +
+                NumeroConta
+                + ", Titular: " +
+                NomeTitular
+                + ", Possui o saldo de R$" +
+                ValorConta.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ Cap05/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Entre com o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string NomeTitular = Console.ReadLine();
+
+            ContaBancaria c = new ContaBancaria(conta, NomeTitular);
+
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opcao = char.Parse(Console.ReadLine());
+
+            if (opcao != null && opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Entre o valor depósito inicial: ");
+                c.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("Dados da conta: \n" + c.ToString());
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para depósito: ");
+            c.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados: \n" + c.ToString());
+
+
+            Console.Write("Entre com um valor para saque: ");
+            c.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados: \n" + c.ToString());
+
+
+
+
+        }
+    }
+}
